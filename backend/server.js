e1f6b5b6e94c32fdaff92d97259addcb9763ff4e
Shloke <shloke.m@lynkit.in>
2024-01-10 13:21:14 +0530

    'endpoint-created'

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const http = require('http');
-const { Server } = require('socket.io');
-const WebSocket = require('ws');
-const url = require('url');
 
 
 const server = http.createServer();
 
 const app = express();
 app.use(bodyParser.json());
-const io = new Server(server);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/public/index.html');
+// });
 
 require("dotenv").config()
 const DB_HOST = process.env.DB_HOST
@@ -33,56 +29,60 @@ const db = mysql.createPool({
     port: DB_PORT
 })
 
+db.getConnection((err, connection) => {
+    if (err) {
+        console.error("Error connecting to the database: ", err);
+    } else {
+        console.log("Successfully connected to the database");
+        connection.release();
+    }
+});
 
 
-// User registration API
-app.post('/user-registration', (req, res) => {
-    const { email, userName, userProfilePic, phoneNumber, address, androidId, fcmToken, dateOfBirth } = req.body;
-    const query = 'SELECT * from user WHERE email = ? OR phoneNumber = ?';
-    db.query(query, [email, phoneNumber], (error, results) => {
+//Route to Add an item to inventory 
+app.post('/addItem', (req, res) => {
+    const { item_id,item_name,quantity,description } = req.body;
+    const query = 'SELECT * from grocery WHERE item_id = ?';
+    db.query(query, [item_id], (error, results) => {
         if (error) {
             return res.status(500).send({ success: false, message: 'Error querying the database', error });
         }
 
         if (results.length) {
-            return res.status(400).send({ success: false, message: 'User already exists, redirecting to login page' });
+            return res.status(400).send({ success: false, message: 'Item already exists' });
         }
-        const newUser = {
-            email, userName, userProfilePic, phoneNumber, address, androidId, fcmToken, dateOfBirth, userStatus: 0, isUserBlocked: false
+        const newItem = {
+            item_id,item_name,quantity,description
         };
 
-        db.query('INSERT INTO user SET ?', newUser, (error, results) => {
+        db.query('INSERT INTO grocery SET ?', newItem, (error, results) => {
             if (error) {
                 return res.status(500).send({ success: false, message: 'Error inserting data into the database', error });
             }
 
-            newUser['userId'] = results.insertId;
-            return res.status(200).send({ success: true, message: 'User registered successfully', user: { newUser } });
+            newItem['_id'] = results.insertId;
+            return res.status(200).send({ success: true, message: 'Item added successfully', item: { newItem } });
         });
     });
 });
 
-// User login API
-app.post('/user-login', (req, res) => {
-    const { email, phoneNumber } = req.body;
-    const query = 'SELECT * FROM user WHERE email = ? OR phoneNumber = ?';
-    db.query(query, [email, phoneNumber], (error, results) => {
+//Route to Get all items from inventory 
+app.get('/getItems', (req,res) => {
+    const query = 'SELECT * FROM grocery';
+    db.query(query, (error, results) => {
         if (error) {
             return res.status(500).send({ success: false, message: "Error querying the database", error });
         }
 
         if (results.length) {
-            return res.status(200).send({ isUserRegistered: true, success: true, message: 'Logging in', user: results[0] });
+            return res.status(200).send({ success: true, message: 'Items fetched successfully!', items: results });
         } else {
-            return res.status(200).send({ isUserRegistered: false, success: false, message: 'User does not exist' });
+            return res.status(200).send({ success: false, message: 'No items found!' });
         }
     })
 })
 
 const port = process.env.PORT || 3000;
 
-// app.get('/', (req, res) => {
-//     res.send("Server is running!");
-// });
 
-server.listen(port, () => console.log(`Server Started on port ${port}...`)).setMaxListeners(10);
+app.listen(port, () => console.log(`Server Started on port ${port}...`)).setMaxListeners(10);
